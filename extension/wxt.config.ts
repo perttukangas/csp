5ae84f91f34f39b3b79179a76cb192f384bbd20b
DeeCaaD <perttu.kangas@hotmail.fi>
2025-10-17 12:06:59 +0300

    Fix manifest

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -5,10 +5,5 @@ export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   manifest: {
     permissions: ['tabs', 'activeTab', 'storage', 'unlimitedStorage'],
-    host_permissions: [
-      'http://localhost:3000/*',
-      'http://localhost:8080/*',
-      'http://127.0.0.1:*/*',
-    ],
   },
 });
